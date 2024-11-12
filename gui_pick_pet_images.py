41b416d12adc3897dbdcd0714566326e05dd5c30
Salmaan Saeed <salsaeed999@gmail.com>
2024-11-12 17:37:41 -0500

    formatted the code

--- gui_pick_pet_images.py
+++ gui_pick_pet_images.py
@@ -100,7 +100,9 @@ def remove_image():
 def save_images():
     if len(picked_image_paths) == 4:
         dest_path_s = "picked_images/pets"
-        file = open("picked_images/pets/picked_pet_images.txt", "a", encoding="utf-8")
+        file = open(
+            "picked_images/pets/picked_pet_images.txt", "a", encoding="utf-8"
+        )
         for i in range(len(picked_image_paths)):
             image_path_s = picked_image_paths[i]
             copied_file_path_s = shutil.copy(image_path_s, dest_path_s)
