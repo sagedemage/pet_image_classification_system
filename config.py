22531b19cf4d446d55b41f02d5a05dfe05b2ea21
Salmaan Saeed <salsaeed999@gmail.com>
2024-11-14 02:34:05 -0500

    Change the batch size and image size. Removed extra comments.

--- config.py
+++ config.py
@@ -7,15 +7,13 @@ Config for:
 
 # CSV datasets
 CSV_DATASET = "dataset/imdb_top_1000.csv"
-# CSV_TRAINING_DATASET = "training_dataset/training_movies.csv"
-# CSV_VALIDATION_DATASET = "validation_dataset/validation_movies.csv"
 CSV_PERSONALIZED_DATASET = "personalized_dataset/recommended_movies.csv"
 
 # The picked movie text file is used to remember what movie the user chose
 PICKED_MOVIE_TEXT_FILE = "personalized_dataset/picked_movie.txt"
 
 # The batch size is used for preparing the data with DataLoaders
-BATCH_SIZE = 4
+BATCH_SIZE = 64
 
 # Image size to resize to
-img_size = (500, 300)
+img_size = (200, 300)
