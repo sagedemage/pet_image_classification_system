7190a0daaec4c5e053a8a4b1b58b837b6ce88fee
Salmaan Saeed <salsaeed999@gmail.com>
2024-11-14 02:38:28 -0500

    changed the structure of the PetClassifier model

--- ml/model.py
+++ ml/model.py
@@ -1,27 +1,23 @@
 """Structure of the Model"""
 
 import torch.nn as nn
-import torch.nn.functional as F
-import torch
 
 
 class PetClassifier(nn.Module):
     """Define a Model for Pet Classification"""
 
     def __init__(self):
-        super(PetClassifier, self).__init__()
-        self.conv1 = nn.Conv2d(3, 4, 5)
-        self.pool = nn.MaxPool2d(2, 8)
-        self.conv2 = nn.Conv2d(4, 8, 5)
-        self.fc1 = nn.Linear(16 * 4 * 4, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 4)
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(3 * 200 * 300, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, 64),
+        )
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1)  # flatten all dimensions except batch
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
