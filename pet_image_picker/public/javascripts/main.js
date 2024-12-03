49336bcd17b6a9e31940cff69f936b5a512febcf
Salmaan Saeed <salsaeed999@gmail.com>
2024-12-03 18:55:32 -0500

    implemented a feature for the web app where the user can save the pet image to the picked_image/pet/ directory

--- pet_image_picker/public/javascripts/main.js
+++ pet_image_picker/public/javascripts/main.js
@@ -1,5 +1,6 @@
 let index = 0
 let length = 0
+let image_file = ""
 
 window.addEventListener("keydown", previousEvent)
 window.addEventListener("keydown", nextEvent)
@@ -15,6 +16,7 @@ async function getImages() {
         const json = await response.json();
         let image_files = json.image_files;
         length = image_files.length-1
+        image_file = "public/" + image_files[index]
         document.getElementById("pet_image").src = image_files[index]
         document.getElementById("image_pos").innerText = `${index} of ${length}` 
     } catch (error) {
@@ -35,6 +37,33 @@ function next() {
     if (index < length) {
         index += 1
         getImages()
+    }
+}
+
+async function save_image() {
+    console.log(image_file)
+
+    const url = "/api/save_image"
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            body: JSON.stringify({image_file: image_file}),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`)
+        }
+
+        const json = await response.json();
+        let image_files = json.image_files;
+        length = image_files.length-1
+        image_file = "public/" + image_files[index]
+        document.getElementById("pet_image").src = image_files[index]
+        document.getElementById("image_pos").innerText = `${index} of ${length}`
+    } catch (error) {
+        console.error(error.message)
     }
 }
 
--- pet_image_picker/routes/api.js
+++ pet_image_picker/routes/api.js
@@ -25,4 +25,16 @@ router.get("/images", function(req, res, next) {
   res.send(json_content)
 })
 
+router.post("/save_image", function(req, res, next) {
+  const dest_dir = "../picked_image/pet/"
+  const image_dest = dest_dir + "pet.jpg"
+  res.setHeader("Content-Type", "application/json");
+  let image_file = req.body.image_file
+  fs.cpSync(image_file, image_dest, { recursive: true })
+  let image_items = image_file.split("/")
+  let image_file_name = image_items[2]
+  const data = "Image: " + image_file_name
+  fs.writeFileSync(dest_dir + "picked_pet_image.txt", data, {flag: "w"})
+})
+
 module.exports = router;
